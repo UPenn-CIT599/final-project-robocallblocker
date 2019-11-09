57d54df5a37bccd233dd65c6357b2500b0a7a770
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-09 18:35:48 -0500

    Created PhoneTest for unit testing on phone class. Currently only tests createPhoneUserWithContacts to ensure contact list is created properly.

---
+++ src/PhoneTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
+
+/***
+ * Tests phone class
+ * 
+ * @author Shawn Choudhury
+ * @author Joshua Chopra
+ * @author Thomas Tee
+ */
+class PhoneTest {
+
+	@Test
+	void testCreatePhoneUserWithContacts() {
+		Phone ph = new Phone();
+		ContactInfoReader cdr = new ContactInfoReader("contacts10.csv");
+		int numberOfContacts = 5;
+		HashMap<String, ContactInfo> usersContactList = ph.createPhoneUserWithContacts(cdr, numberOfContacts);
+		// number of key-value pairs should b equal to numberofContacts variable
+		assertEquals(numberOfContacts, usersContactList.size()); 
+	}
+
+}
