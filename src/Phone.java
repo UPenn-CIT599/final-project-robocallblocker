529a30d2ce4d57f39db79d639d0c82c17730fe24
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-21 17:30:00 -0500

    used create incoming call method to set phone's display for name, phone number, etc so it is simply passed to GUI class when method is called on a phone object. Moved number of contacts to phone class well

--- src/Phone.java
+++ src/Phone.java
@@ -5,22 +5,30 @@ import java.util.HashMap;
  * that the user makes use of and receives incoming calls, decides to continue
  * to receive calls, or turns off phone.
  * 
- * @author Shawn Choudhury
- * @author Joshua Chopra
- * @author Thomas Tee
+ * @author ShawnChoudhury
+ * @author JoshuaChopra
+ * @author ThomasTee
  */
 public class Phone {
 
+	// store contents from CSV file in phone class to maintain all contacts
 	private ContactInfoReader allContacts = new ContactInfoReader("contacts10.csv");
-	// store contents from CSV file in phone class to maintain all potential contacts. 
 	private HashMap<String, ContactInfo> allContactsInHashMap = allContacts.getContactInfoMap();
 	private SpamAlgorithm spamAlgoForPhone = new SpamAlgorithm();
 	private String displayIncomingCallerPhoneNumber;
 	private String displayIncomingCallerName;
 	private boolean incomingCallSpam; // true if is spam, false if not
-	private HashMap<String, ContactInfo> usersContacts; // used to get users contact list 
+	private HashMap<String, ContactInfo> usersContacts; // used to get users contact list
+	/***
+	 * Default number of contacts a phone user has. 
+	 * Could instead incorporate this into the GUI and 
+	 * let the person using the GUI pick from a number 
+	 * in a combo-box. Removed from GUI class to avoid 
+	 * creating another instance variable that should 
+	 * belong in the phone class. 
+	 */
+	private final int numberOfContactsForUser = 5;
 
-	
 	/***
 	 * Method to create a phone user that has a list of contacts (subset of the
 	 * total contacts list) and the contact list will be used for checking against
@@ -28,28 +36,34 @@ public class Phone {
 	 * - set private.
 	 * 
 	 * @param allContactsInHashMap - pass in HashMap of contacts from CSV
-	 * @param numberOfContacts        - desired number of contacts for user
+	 * @param numberOfContacts     - desired number of contacts for user
 	 * @return HashMap of user's contacts (contact for key, contactInfo for values)
 	 */
-	HashMap<String, ContactInfo> createPhoneUserWithContacts(HashMap<String, ContactInfo> allContactsInHashMap, int numberOfContacts) {
+	public HashMap<String, ContactInfo> createPhoneUserWithContacts(HashMap<String, ContactInfo> allContactsInHashMap,
+			int numberOfContacts) {
 		UsersContactList phoneUsersContactsList = new UsersContactList(allContactsInHashMap, numberOfContacts);
 		/*
 		 * returns a HashMap with the phone user's contacts names as key, and all info
 		 * as ContactInfo object
 		 */
-		return phoneUsersContactsList.getContactList();
+		usersContacts = phoneUsersContactsList.getContactList();
+		return usersContacts;
 	}
 
 	/***
 	 * Creates an incoming call for the current phone instance and gets contact
 	 * information associated with the caller
 	 * 
-	 * @param contactsDataMap - pass in hashmap from ContactInfoReader
+	 * @param usersContactList - pass in hashmap containing user's contact list,
+	 *                         used to check if incoming call is spam
 	 * @return - ContactInfo associated with caller
 	 */
-	ContactInfo createIncomingCallGetContactInfoForCaller(HashMap<String, ContactInfo> contactsDataMap) {
-		IncomingCall call = new IncomingCall(contactsDataMap);
-		return call.getIncomingCallerInfo(); // return info of incoming caller
+	public void createIncomingCallGetContactInfoForCaller(HashMap<String, ContactInfo> usersContactList) {
+		IncomingCall call = new IncomingCall(allContactsInHashMap);
+		ContactInfo forCaller = call.getIncomingCallerInfo();
+		setDisplayIncomingCallerName(forCaller.getName());
+		setDisplayIncomingCallerPhoneNumber(forCaller.getPhoneNumbers());
+		setIncomingCallSpamOrNotSpam(getSpamAlgoForPhone().compareAgainst(forCaller, usersContactList));
 	}
 
 	public ContactInfoReader getAllContacts() {
@@ -67,11 +81,11 @@ public class Phone {
 	public void setDisplayIncomingCallerPhoneNumber(String displayIncomingCallerPhoneNumber) {
 		this.displayIncomingCallerPhoneNumber = displayIncomingCallerPhoneNumber;
 	}
-	
+
 	public String getDisplayIncomingCallerName() {
 		return displayIncomingCallerName;
 	}
-	
+
 	public void setDisplayIncomingCallerName(String displayIncomingCallerName) {
 		this.displayIncomingCallerName = displayIncomingCallerName;
 	}
@@ -83,7 +97,7 @@ public class Phone {
 	public void setIncomingCallSpamOrNotSpam(boolean incomingCallSpam) {
 		this.incomingCallSpam = incomingCallSpam;
 	}
-	
+
 	public HashMap<String, ContactInfo> getAllContactsInHashMap() {
 		return allContactsInHashMap;
 	}
@@ -91,7 +105,7 @@ public class Phone {
 	public void setAllContactsInHashMap(HashMap<String, ContactInfo> allContactsInHashMap) {
 		this.allContactsInHashMap = allContactsInHashMap;
 	}
-	
+
 	public HashMap<String, ContactInfo> getUsersContacts() {
 		return usersContacts;
 	}
@@ -100,39 +114,8 @@ public class Phone {
 		this.usersContacts = usersContacts;
 	}
 
-	
+	public int getNumberOfContactsForUser() {
+		return numberOfContactsForUser;
+	}
 
-	/***
-	 * Allows use to use our phone object with a "user" that has a list of contacts,
-	 * and the phone can receive calls and then the user is told whether or not the
-	 * incoming call is spam, and is prompted to take another call or not which will
-	 * determine if the phone continues to be "used" i.e., if the program continues
-	 * to run
-	 * 
-	 * @param contactsData     - pass in object that contains all data from CSV
-	 * @param numberOfContacts - desired number of contacts for phone user
-	 * @param spamAlgo         - use SpamAlgorithm object
-	 * @return - nothing, used as a run method for the phone object.
-	 */
-//	public void usePhone(ContactInfoReader contactsData, int numberOfContactsForUser, SpamAlgorithm spamAlgo) {
-//		HashMap<String, ContactInfo> usersContactList = createPhoneUserWithContacts(contactsData,
-//				numberOfContactsForUser);
-//		while (getNextCall) {
-//			ContactInfo forIncomingCaller = createIncomingCallGetContactInfoForCaller(contactsData.getContactInfoMap());
-//			boolean isSpam = spamAlgo.compareAgainst(forIncomingCaller, usersContactList);
-//			if (isSpam) {
-//				System.out.println("Phone call is likely spam" + '\n' + "Take next incoming call?");
-//			} else {
-//				System.out.println("Phone call is most likely not spam" + '\n' + "Take next incoming call?");
-//			}
-//		}
-//	}
-
-//	public static void main(String[] args) {
-//		ContactInfoReader contactsFromFile = new ContactInfoReader("contacts10.csv");
-//		SpamAlgorithm spamAlgo = new SpamAlgorithm();
-//		Phone temp = new Phone();
-//		int numberOfContactsForUser = 5;
-//		temp.usePhone(contactsFromFile, numberOfContactsForUser, spamAlgo);
-//	}
 }
