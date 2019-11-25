60cac842cbab4287bc3e7075187a02e32c6708fb
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-25 16:12:28 -0500

    Had to create a method to handle blanks for phone numbers as values for ContactInfo objects in the HashMap, since incoming calls were being created and on GUI it was showing up as 0 is calling which didn't make any sense.

--- src/IncomingCall.java
+++ src/IncomingCall.java
@@ -1,31 +1,64 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class IncomingCall {
 
-	ContactInfo incomingCallerInfo;
+	private ContactInfo incomingCallerInfo;
 
 	/***
-	 * Shuffle Contact Info to create a random incoming call from CSV of contacts
+	 * Creates an incoming call that is passed to a "Phone" instance which then uses
+	 * SpamAlgorithm to check if this incoming call is spam. We used a "cleaned"
+	 * version of allContacts HashMap from ContactInfoDataReader since we do not
+	 * want to generate calls with a blank phone number. We shuffle our list of
+	 * names of the contacts then take the first one to create a call.
 	 * 
-	 * @param allContactsFromFile - takes in hashmap created when we read in CSV
+	 * @param allContactsFromFile - takes in HashMap created when we read in CSV
 	 */
 	public IncomingCall(HashMap<String, ContactInfo> allContactsFromFile) {
-		ArrayList<String> phoneNumbers = new ArrayList<String>(allContactsFromFile.keySet());
+		HashMap<String, ContactInfo> allContactsCleaned = removeBlankPhoneNumbersFromMapUsedToCreateCalls(
+				allContactsFromFile);
+		ArrayList<String> contactNames = new ArrayList<String>(allContactsCleaned.keySet());
+		Collections.shuffle(contactNames);
+		incomingCallerInfo = allContactsCleaned.get(contactNames.get(0));
+	}
+
+	/***
+	 * Helper method used to ensure we never use a key-value pair that has a
+	 * ContactInfo object where the phone number of that ContactInfo object is
+	 * blank, AKA "0", since we set blanks to 0 when reading in the data.
+	 * Intuitively, wouldn't make any sense to receive a call from a phone number of
+	 * "0" or blank.
+	 * 
+	 * @param allContacts hashMap we create upon reading in contacts from CSV
+	 * @return cleaned map of contacts used to generate calls (no blank phone #s)
+	 * 
+	 */
+	public HashMap<String, ContactInfo> removeBlankPhoneNumbersFromMapUsedToCreateCalls(
+			HashMap<String, ContactInfo> allContacts) {
 		/*
-		 * Given that data in CSV contains blanks for some phone numbers 
-		 * in the file which are filled with 0s, we don't want to ever 
-		 * create an incoming call from our keySet with the phone number
-		 * "0". 
+		 * use iterator since we can't iterate over a keySet and remove key-value pair
+		 * simultaneously or java throws ConcurrentModificationException
 		 */
-		for (String phoneNumber : phoneNumbers) {
-			if(phoneNumber.equals("0")) {
-				phoneNumbers.remove(phoneNumber);
+
+		Iterator<Map.Entry<String, ContactInfo>> iterator = allContacts.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Map.Entry<String, ContactInfo> nameWithContactInfo = iterator.next();
+			/*
+			 * if value (ContactInfo object) associated with key (name of contact) has
+			 * variable value for phone number as "0" then remove it from our map. Else keep
+			 * it.
+			 */
+			if (nameWithContactInfo.getValue().getPhoneNumber().equals("0")) {
+				iterator.remove();
 			}
 		}
-		Collections.shuffle(phoneNumbers);
-		incomingCallerInfo = allContactsFromFile.get(phoneNumbers.get(0));
+		return allContacts;
+
 	}
 
 	/***
