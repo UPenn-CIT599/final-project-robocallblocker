506883c9b76b42d4892f65477567d9a542cc55c4
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-25 18:52:47 -0500

    Substantial changes made here. Took out all grid layouts and flow layouts and instead used a North, South, East, West Layout. Added components based on their relative position. Set new sizing of GUI. Changed background, added text to welcome user as well as instructions for when a call comes in

--- src/CallBlockerGUI_JoshEdits.java
+++ src/CallBlockerGUI_JoshEdits.java
@@ -15,17 +15,23 @@ import javax.swing.*;
 public class CallBlockerGUI_JoshEdits implements Runnable {
 
 	// Images used in GUI
-	private ImageIcon accept = new ImageIcon("accept_button.PNG");
-	private ImageIcon decline = new ImageIcon("decline_button.PNG");
+	private ImageIcon accept = new ImageIcon("acceptCall.gif");
+	private ImageIcon decline = new ImageIcon("declineCall.gif");
 //	private ImageIcon start = new ImageIcon("startButton.jpg"); // TODO figure out how to incorporate
 
 	// Frame and buttons used in GUI
-	private JFrame frame = new JFrame("Call Blocker Program"); // TODO make a better name
-	private JButton acceptButton = new JButton(accept);
-	private JButton declineButton = new JButton(decline);
+	JFrame frame = new JFrame("Call Blocker Program"); // TODO make a better name
+	private JButton acceptButton = new JButton("Accept",
+			new ImageIcon((accept.getImage()).getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH)));
+//	private JButton acceptButton = new JButton(accept);
+//	private JButton declineButton = new JButton(decline);
+	private JButton declineButton = new JButton("Decline",
+			new ImageIcon((decline.getImage()).getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH)));
 
-//	JButton startButton = new JButton(new ImageIcon(((new ImageIcon("startButton.jpg")).getImage())
-//	.getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH))); //TODO resize images 
+//	JButton startButton = new JButton(new ImageIcon(
+//			((new ImageIcon("startButton.jpg")).getImage()).getScaledInstance(40, 40, java.awt.Image.SCALE_SMOOTH))); // TODO
+//																														// resize
+//																														// images
 //	private JButton startButton = new JButton(start);
 
 	/*
@@ -35,29 +41,37 @@ public class CallBlockerGUI_JoshEdits implements Runnable {
 	private static Phone phone = new Phone();
 
 	public static void main(String[] args) {
+
 		phone.createPhoneUserWithContacts(phone.getAllContactsInHashMap(), phone.getNumberOfContactsForUser());
 		SwingUtilities.invokeLater(new CallBlockerGUI_JoshEdits());
 	}
 
 	@Override
 	public void run() {
-		JPanel outerPanel = new JPanel(new GridLayout(2, 1)); // overall outer panel of program
-		JPanel innerBottomPanel = new JPanel(new GridLayout(1, 2));
-		JPanel upperTopPanel = new JPanel(new FlowLayout());
+
+		// create panels to hold labels and buttons
+		JPanel backgroundPanel = new JPanel(); // overall outer panel of program
+		JPanel holdAcceptAndDecline = new JPanel(); // hold accept & decline 
+		JPanel panelForIncomingCall = new JPanel(); // details for calls
+		
 //		JPanel middlePanel = new JPanel(new GridLayout(1,1)); // will display start button
+
 		
-		upperTopPanel.setBackground(Color.BLACK); // set background to black
+		panelForIncomingCall.setBackground(Color.BLACK); // set background to black
+		
+		// welcome user to the program initially 
 		JLabel incomingCallDetails = new JLabel();
-		
-		incomingCallDetails.setText("<html>" + "Please click accept to begin" + "<br>" + "</html>");
+		incomingCallDetails.setFont(new Font("Helvetica", Font.BOLD, 17));
+		incomingCallDetails.setText("<html>" + "Welcome to Robo-Call Blocker Program." + "<br>" + "<br>"
+				+ "Please click the accept button to recieve your first call." + "</html>");
 		incomingCallDetails.setForeground(Color.WHITE); // set color of label
-
-		// create all buttons
-
-		// should incorporate this to start the program, then change the screen when
-		// user clicks start
-
-		// Create actionListeners for events of pressing accept or decline
+		incomingCallDetails.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		
+		JLabel userInstructions = new JLabel("");
+		userInstructions.setForeground(Color.WHITE); // set color of label
+		userInstructions.setFont(new Font("Helvetica", Font.BOLD, 17));
+		userInstructions.setHorizontalAlignment(SwingConstants.CENTER);
 
 		/*
 		 * If accept button was pressed to begin the program, then display the
@@ -69,12 +83,16 @@ public class CallBlockerGUI_JoshEdits implements Runnable {
 				phone.createIncomingCallDisplayOnPhoneScreenGUI(phone.getUsersContacts());
 				if (phone.isIncomingCallSpam()) {
 					incomingCallDetails.setText("<html>" + phone.getDisplayIncomingCallerPhoneNumber() + " is calling."
-							+ "<br>" + phone.getDisplayIncomingCallerPhoneNumber() + " is likely spam."
-							+ "<br>Accept or Decline? </html>");
+							+ "<br>" + phone.getDisplayIncomingCallerPhoneNumber() + " is likely spam." + "<br>"
+							+ "<br>Accept or Decline?");
+					userInstructions.setText("<html>" + "<br>If you accept, the program will continue to your next call."
+							+ "<br>If you decline, the program will exit." + "</html>");
 				} else {
 					incomingCallDetails.setText("<html>" + phone.getDisplayIncomingCallerName() + " is calling."
-							+ "<br>" + phone.getDisplayIncomingCallerPhoneNumber() + " is likely not spam."
-							+ "<br>Accept or Decline? </html>");
+							+ "<br>" + phone.getDisplayIncomingCallerPhoneNumber() + " is likely not spam." + "<br>"
+							+ "<br>Accept or Decline?");
+					userInstructions.setText("<html>" + "<br>If you accept, the program will continue to your next call."
+							+ "<br>If you decline, the program will exit." + "</html>");
 				}
 			}
 		});
@@ -99,22 +117,60 @@ public class CallBlockerGUI_JoshEdits implements Runnable {
 		/*
 		 * Add the components together in this order: Frame > OuterPanel >
 		 * [InnerTopPanel, > Label InnerBottomPanel] > Buttons
-		 */
-		frame.add(outerPanel);
-		outerPanel.setBackground(Color.BLACK);
-		outerPanel.add(upperTopPanel);
-//		outerPanel.add(middlePanel);
-		upperTopPanel.add(incomingCallDetails);
-//		upperTopPanel.add(startProgram);
-
-		outerPanel.add(innerBottomPanel);
-		innerBottomPanel.add(acceptButton);
-		innerBottomPanel.add(declineButton);
+		 */ 
+		
+		//OLD LAYOUT
+//		frame.add(outerPanel, BorderLayout.CENTER);
+//		outerPanel.setBackground(Color.BLACK);
+//		outerPanel.add(upperTopPanel, BorderLayout.NORTH);
+////		outerPanel.add(middlePanel);
+//		upperTopPanel.add(incomingCallDetails);
+////		upperTopPanel.add(startProgram);
+//
+//		outerPanel.add(innerBottomPanel, BorderLayout.SOUTH);
+//		innerBottomPanel.add(acceptButton);
+//		innerBottomPanel.add(declineButton);
 //		middlePanel.add(startButton);
 
+		/*
+		 * Set background of button to black, text to white hide border, and place text
+		 * below image, but centered
+		 */
+		declineButton.setBackground(Color.BLACK);
+		declineButton.setForeground(Color.WHITE);
+		declineButton.setBorderPainted(false);
+		declineButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		declineButton.setHorizontalTextPosition(SwingConstants.CENTER);
+
+		/*
+		 * Set background of button to black, text to white hide border, and place text
+		 * below image, but centered
+		 */
+		acceptButton.setBackground(Color.BLACK);
+		acceptButton.setForeground(Color.WHITE);
+		acceptButton.setBorderPainted(false);
+		acceptButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		acceptButton.setHorizontalTextPosition(SwingConstants.CENTER);
+
+		backgroundPanel.setBackground(Color.BLACK);
+		holdAcceptAndDecline.setBackground(Color.BLACK);
+
+		frame.add(backgroundPanel, BorderLayout.CENTER);
+		backgroundPanel.add(userInstructions);
+		
+		
+		panelForIncomingCall.add(incomingCallDetails);
+		frame.add(panelForIncomingCall, BorderLayout.NORTH);
+		
+		holdAcceptAndDecline.add(acceptButton);
+		holdAcceptAndDecline.add(declineButton);
+		frame.add(holdAcceptAndDecline, BorderLayout.SOUTH);
+
 		// required pieces of code
+		frame.setSize(600, 700); // sizes frame to whatever we want
+		frame.setLocationRelativeTo(null); // puts at center of screen
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
+//		frame.pack();
 		frame.setVisible(true);
 	}
 }
