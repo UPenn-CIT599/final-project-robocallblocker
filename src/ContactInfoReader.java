ed71325730cf8ca9a14b394efefaefce4a9262a2
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-24 22:11:59 -0500

    Edited class to handle blank entries in CSV file, using RegEx and -1 in .split() method when reading in a line, to continue to search for instances of a , within the file, to be used with larger CSV. Was running into index out of bounds issue without it

--- src/ContactInfoReader.java
+++ src/ContactInfoReader.java
@@ -21,20 +21,30 @@ public class ContactInfoReader {
 	// Constructor
 	public ContactInfoReader(String filename) {
 
-		// try with resources to automatically close out scanner object 
+		// try with resources to automatically close out scanner object
 		try (Scanner contactsDataReader = new Scanner(new File(filename));) {
 
-			// skip the column headers in first line 
-			contactsDataReader.nextLine(); 
+			// skip the column headers in first line
+			contactsDataReader.nextLine();
 			while (contactsDataReader.hasNextLine()) {
 				String line = contactsDataReader.nextLine();
-				String[] columnData = line.split(",");
+				/*
+				 * need negative integer limit to keep applying pattern to empty strings in file
+				 * use RegEx with negative lookahead to capture blanks after a "," and not count
+				 * a "," as an actual input to the string [], and if a data entry has a
+				 * whitespace character after a comma, we want that whole data entry as its own
+				 * string and do not want to split it.
+				 */
+				String[] columnData = line.split(",(?! )", -1);
+				if (columnData.length != 12) {
+					System.out.println(columnData);
+				}
 				// Concatenate first and last name
-				String name = columnData[0] + " " + columnData[1];
-				String number = columnData[8];
-				String email = columnData[9];
-				String socialMediaHandle = columnData[11];
-				String address = columnData[3];
+				String name = checkIfBlankThenFill(columnData, 0) + " " + checkIfBlankThenFill(columnData, 1);
+				String number = checkIfBlankThenFill(columnData, 8);
+				String email = checkIfBlankThenFill(columnData, 9);
+				String socialMediaHandle = checkIfBlankThenFill(columnData, 11);
+				String address = checkIfBlankThenFill(columnData, 3);
 				// fill up the constructor
 				ContactInfo contact = new ContactInfo(name, number, email, socialMediaHandle, address);
 				// put the contact info in to the HashMap
@@ -49,12 +59,33 @@ public class ContactInfoReader {
 	}
 
 	/***
-	 * Get HashMap with key as contact's name, value as contactInfo object 
-	 * that has all attributes of a contact
-	 * @return HashMap of all contacts from file 
+	 * Get HashMap with key as contact's name, value as contactInfo object that has
+	 * all attributes of a contact
+	 * 
+	 * @return HashMap of all contacts from file
 	 */
 	public HashMap<String, ContactInfo> getContactInfoMap() {
 		return contactInfoMap;
+	}
+
+	/***
+	 * Check if a data entry is blank, and just simply fill as string with 0 if so.
+	 * Will be useful if necessary for parsing ints, etc. given that we can modify
+	 * our read in process with flexibility from the method.
+	 * 
+	 * @param columnData
+	 * @param column
+	 * @return
+	 */
+	public String checkIfBlankThenFill(String[] columnData, int column) {
+		if (columnData[column].isEmpty()) {
+			return "0";
+		} else
+			return columnData[column];
+	}
+	
+	public static void main(String[] args) {
+		ContactInfoReader cdr = new ContactInfoReader("contacts100.csv");
 	}
 
 }
