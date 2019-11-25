9e5563058da5b0d38da3332edf4fb696ff9f26ef
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-25 16:00:17 -0500

    Edited SpamAlgo so if user has the incoming caller's phone number, automatically not marked as spam. Made isSpam a global instance variable instead of local to compareAgainst method, for better design.

--- src/SpamAlgorithm.java
+++ src/SpamAlgorithm.java
@@ -1,33 +1,52 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/***
+ * This class is the algorithm used to determine if an incoming call is likely
+ * spam, or not spam. It uses a scoring system based on if a phone user has an
+ * incoming caller's name, email, address, social media handle, or phone number
+ * in their phone. We make the assumption that this data is somewhere in the
+ * phone, (i.e., if the phone number isn't present then the social media handle
+ * could be present in an app, or the name could be in text messages app, or
+ * email could be in mail app).
+ * 
+ * @author ShawnChoudhury
+ * @author JoshuaChopra
+ * @author ThomasTee
+ */
 public class SpamAlgorithm {
+
+	// tells us if a call is spam or not
+	boolean isSpam;
 
 	public boolean compareAgainst(ContactInfo incoming, HashMap<String, ContactInfo> User) {
 
 		int score = 0;
-		Boolean spam = false;
-		ArrayList<String> Namekeys = new ArrayList<String>(User.keySet());
+
+		ArrayList<String> namesOfContacts = new ArrayList<String>(User.keySet());
 
 		// Go through User Contact List and Compare name, email, address, social media,
 		// and phone number
-		for (String key : Namekeys) {
+		for (String name : namesOfContacts) {
 
-			if (incoming.getName().equals(User.get(key).getName())) {
-				score++;
-//				System.out.println("Caller is " + incoming.getName());
-			}
-			if (incoming.getEmailAddress().equals(User.get(key).getEmailAddress())) {
+			if (incoming.getName().equals(User.get(name).getName())) {
 				score++;
 			}
-			if (incoming.getAddress().equals(User.get(key).getAddress())) {
+			if (incoming.getEmailAddress().equals(User.get(name).getEmailAddress())) {
 				score++;
 			}
-			if (incoming.getSocialMediaHandle().equals(User.get(key).getSocialMediaHandle())) {
+			if (incoming.getAddress().equals(User.get(name).getAddress())) {
 				score++;
 			}
-			if (incoming.getPhoneNumbers().equals(User.get(key).getPhoneNumbers())) {
+			if (incoming.getSocialMediaHandle().equals(User.get(name).getSocialMediaHandle())) {
 				score++;
+			}
+			/*
+			 * if phone number is in contact book of user, automatically not spam, set above
+			 * threshold of 2
+			 */
+			if (incoming.getPhoneNumber().equals(User.get(name).getPhoneNumber())) {
+				score += 3;
 			}
 		}
 
@@ -35,8 +54,11 @@ public class SpamAlgorithm {
 
 		// If there are only 2 or less matches, then set as Spam
 		if (score <= 2) {
-			spam = true;
+			isSpam = true;
 		}
-		return spam;
+		else {
+			isSpam = false;
+		}
+		return isSpam;
 	}
 }
