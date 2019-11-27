3fbdf8c8fcf80339d2446e4e7dd2a77a4d660c07
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-26 23:02:22 -0500

    Edited reader to take in every single column of CSV and use that attribute to pass to ContactInfo object created within class

--- src/ContactInfoReader.java
+++ src/ContactInfoReader.java
@@ -17,8 +17,8 @@ public class ContactInfoReader {
 	// HashMap of contacts with name as key and ContactInfo as value.
 	private HashMap<String, ContactInfo> contactInfoMap = new HashMap<>();
 
-	int index = 0; 
-	
+	int uniqueID = 0;
+
 	// Constructor
 	public ContactInfoReader(String filename) {
 
@@ -40,31 +40,40 @@ public class ContactInfoReader {
 				// Concatenate first and last name
 				String name = checkIfBlankThenFill(columnData, 0) + " " + checkIfBlankThenFill(columnData, 1);
 				// we don't want a last name displayed on GUI with a "0", so check if last char
-				// is 0.
+				// is 0 if the last name was blank when read in.
 				if (name.charAt(name.length() - 1) == '0') {
 					name = name.substring(0, name.length() - 1);
 				}
 				String number = checkIfBlankThenFill(columnData, 8);
-				String email = checkIfBlankThenFill(columnData, 10);
-				// REMEMBER that contacts100.csv has 11 columns, and contacts10.csv has 12 (0 indexing)
+				/*
+				 * REMEMBER that contacts100.csv has 11 columns, and contacts10.csv has 12 (when
+				 * counting using 0 indexing, i.e., first column is 0) so email here is 9, in the
+				 * other file it's 10. 
+				 */
+				String email = checkIfBlankThenFill(columnData, 9);
+				/*
+				 * REMEMBER that contacts100.csv has 11 columns, and contacts10.csv has 12 (when
+				 * counting using 0 indexing, i.e., first column is 0)
+				 */
 				String socialMediaHandle = checkIfBlankThenFill(columnData, 11);
+				String company = checkIfBlankThenFill(columnData, 2);
 				String address = checkIfBlankThenFill(columnData, 3);
+				String city = checkIfBlankThenFill(columnData, 4);
+				String county = checkIfBlankThenFill(columnData, 5);
+				String state = checkIfBlankThenFill(columnData, 6);
+				String zipCode = checkIfBlankThenFill(columnData, 7);
+				
 				// fill up the constructor
-				ContactInfo contact = new ContactInfo(name, number, email, socialMediaHandle, address);
+				ContactInfo contact = new ContactInfo(name, number, email, socialMediaHandle, address, company, city, county, state, zipCode);
 				// put the contact info in to the HashMap
-				
-				
-				contactInfoMap.put(Integer.toString(index), contact);
-				index += 1;
-				
-				
+
+				contactInfoMap.put(Integer.toString(uniqueID), contact);
+				uniqueID += 1; // create new uniqueID for each contact read in
 			}
 
 		} catch (FileNotFoundException e) {
 			System.out.println("Uh-oh! Error processing file; please check the name and file location.");
-
 		}
-
 	}
 
 	/***
