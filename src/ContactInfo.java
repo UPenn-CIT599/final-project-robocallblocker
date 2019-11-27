1718faf3371fd44bd7e5cd75ac154537f5ed2853
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-26 23:02:45 -0500

    Edited ContactInfo to handle all columns of data from CSV

--- src/ContactInfo.java
+++ src/ContactInfo.java
@@ -22,19 +22,43 @@ public class ContactInfo {
 
 	private String address;
 
-	// Constructor
-	public ContactInfo(String name, String number, String email, String socialMediaHandle, String address) {
+	private String company;
 
+	private String city;
+
+	private String county;
+
+	private String state;
+
+	private String zipCode;
+
+	/***
+	 * Create an object to hold the contact info of a person, read in from our
+	 * contacts CSV file.
+	 * 
+	 * @param name (this will be both first and last)
+	 * @param number - phone number 
+	 * @param email address
+	 * @param socialMediaHandle name on social media 
+	 * @param address 
+	 * @param company
+	 * @param city
+	 * @param county
+	 * @param state
+	 * @param zipCode
+	 */
+	public ContactInfo(String name, String number, String email, String socialMediaHandle, String address,
+			String company, String city, String county, String state, String zipCode) {
 		this.name = name;
-
 		this.phoneNumber = number;
-
 		this.emailAddress = email;
-
 		this.socialMediaHandle = socialMediaHandle;
-
 		this.address = address;
-
+		this.company = company;
+		this.city = city;
+		this.county = county;
+		this.state = state;
+		this.zipCode = zipCode;
 	}
 
 	/***
@@ -90,13 +114,58 @@ public class ContactInfo {
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	
+
 	/**
 	 * Overriden toString() method to print the ContactInfo object contents
 	 * Otherwise, it would print the memory location
 	 */
 	public String toString() {
-		return name + " " + phoneNumber + " " + emailAddress + " " + socialMediaHandle + " " +  address;
+		return name + " " + phoneNumber + " " + emailAddress + " " + socialMediaHandle + " " + address;
+	}
+
+	/***
+	 * Returns company associated with contact
+	 * 
+	 * @return company employed at (e.g., Facebook)
+	 */
+	public String getCompany() {
+		return company;
+	}
+
+	/***
+	 * Returns city contact lives in
+	 * 
+	 * @return city (e.g., Orlando)
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/***
+	 * Returns county contact lives in
+	 * 
+	 * @return county (e.g., Warren)
+	 */
+	public String getCounty() {
+		return county;
+	}
+
+	/***
+	 * Returns state contact lives in
+	 * 
+	 * @return state (e.g., NY)
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/***
+	 * Returns zipCode for contact's address
+	 * 
+	 * @return zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
 	}
 
 }
