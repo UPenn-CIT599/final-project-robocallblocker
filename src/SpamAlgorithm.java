98e10fd7a93f140c8db42a26d22d9931d8b8eb3b
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-26 23:04:10 -0500

    Added fields for scoring for company, city, county, state, zipcode. Gave phone number a score of 2 instead of 1. Set threshold of score to 3 for not a spam call

--- src/SpamAlgorithm.java
+++ src/SpamAlgorithm.java
@@ -18,60 +18,76 @@ import java.util.Set;
  */
 public class SpamAlgorithm {
 
-	
+	private boolean isSpam;
 
 	public boolean compareAgainst(ContactInfo incoming, HashMap<String, ContactInfo> User) {
 
 		int score = 0;
-		
-		boolean spam = true;
-
-		ArrayList<String> Namekeys = new ArrayList<String>(User.keySet());
+		ArrayList<String> uniqueIDsOfContacts = new ArrayList<String>(User.keySet());
 
 		// Go through User Contact List and Compare name, email, address, social media,
 		// and phone number
-		
-		// System.out.println("\n" + incoming);
-		
-	
-		
-		for (String key : Namekeys) {
 
-			score = 0;
-			
-			if (incoming.getName().equals(User.get(key).getName()) && !incoming.getName().equals("0")) {
+		// System.out.println("\n" + incoming);
+
+		for (String uniqueID : uniqueIDsOfContacts) {
+			if (incoming.getName().equals(User.get(uniqueID).getName()) && !incoming.getName().equals("0")) {
 				score++;
-			//	System.out.println("Caller is " + incoming.getName());
+				// System.out.println("Caller is " + incoming.getName());
 			}
-			if (incoming.getEmailAddress().equals(User.get(key).getEmailAddress())
+			if (incoming.getEmailAddress().equals(User.get(uniqueID).getEmailAddress())
 					&& !incoming.getEmailAddress().equals("0")) {
 				score++;
-			//	System.out.println("Email is " + incoming.getEmailAddress());
+				// System.out.println("Email is " + incoming.getEmailAddress());
 			}
-			if (incoming.getAddress().equals(User.get(key).getAddress()) && !incoming.getAddress().equals("0")) {
+			if (incoming.getAddress().equals(User.get(uniqueID).getAddress()) && !incoming.getAddress().equals("0")) {
 				score++;
-			//	System.out.println("Address is " + incoming.getAddress());
+				// System.out.println("Address is " + incoming.getAddress());
 			}
-			if (incoming.getSocialMediaHandle().equals(User.get(key).getSocialMediaHandle())
+			if (incoming.getSocialMediaHandle().equals(User.get(uniqueID).getSocialMediaHandle())
 					&& !incoming.getSocialMediaHandle().equals("0")) {
 				score++;
-			//	System.out.println("Social Media is " + incoming.getSocialMediaHandle());
+				// System.out.println("Social Media is " + incoming.getSocialMediaHandle());
 			}
-			if (incoming.getPhoneNumber().equals(User.get(key).getPhoneNumber())
+			if (incoming.getPhoneNumber().equals(User.get(uniqueID).getPhoneNumber())
 					&& !incoming.getPhoneNumber().equals("0")) {
-				score++;
-			//	System.out.println("Phone number is " + incoming.getPhoneNumber());
+				score += 2; // 2 points if phone number is in 
+				// System.out.println("Phone number is " + incoming.getPhoneNumber());
 			}
-
-			
-			if (score == 5) {
-				spam = false;
-				System.out.println("The number of contact info matches is " + score + "\n");
-			} 		
+			if (incoming.getCompany().equals(User.get(uniqueID).getCompany())
+					&& !incoming.getCompany().equals("0")) {
+				score++;
+				// System.out.println("Company is " + incoming.getPhoneNumber());
+			}
+			if (incoming.getCity().equals(User.get(uniqueID).getCity())
+					&& !incoming.getCity().equals("0")) {
+				score++;
+				// System.out.println("Company is " + incoming.getPhoneNumber());
+			}
+			if (incoming.getCounty().equals(User.get(uniqueID).getCounty())
+					&& !incoming.getCounty().equals("0")) {
+				score++;
+				// System.out.println("Company is " + incoming.getPhoneNumber());
+			}
+			if (incoming.getState().equals(User.get(uniqueID).getState())
+					&& !incoming.getState().equals("0")) {
+				score++;
+				// System.out.println("Company is " + incoming.getPhoneNumber());
+			}
+			if (incoming.getZipCode().equals(User.get(uniqueID).getZipCode())
+					&& !incoming.getZipCode().equals("0")) {
+				score++;
+				// System.out.println("Company is " + incoming.getPhoneNumber());
+			}
 			
 		}
-			
-		
-		return spam;
+		if (score >= 3) {
+			isSpam = false;
+		} else {
+			isSpam = true;
+		}
+		// print this out regardless of the conditional, for testing purposes.
+		System.out.println("The number of contact info matches is " + score + "\n");
+		return isSpam;
 	}
 }
