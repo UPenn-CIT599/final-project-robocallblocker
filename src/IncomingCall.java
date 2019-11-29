f3660ba7e819657248f23ec827c49833002d0988
Joshua Chopra <jchopra@seas.upenn.edu>
2019-11-29 00:57:58 -0500

    Had to change this and remove the method that got rid of contacts with blank phone numbers. Made more sense to have that in the ContactInfoReader since an incoming call class shouldn't be creating a contact list, and made sense for unit testing. had to edit multiple classes as a result.

--- src/IncomingCall.java
+++ src/IncomingCall.java
@@ -20,46 +20,11 @@ public class IncomingCall {
 	 * @param allContactsFromFile - takes in HashMap created when we read in CSV
 	 */
 	public IncomingCall(HashMap<String, ContactInfo> allContactsFromFile) {
-		HashMap<String, ContactInfo> allContactsCleaned = removeBlankPhoneNumbersFromMapUsedToCreateCalls(
-				allContactsFromFile);
-		ArrayList<String> contactNames = new ArrayList<String>(allContactsCleaned.keySet());
+		ArrayList<String> contactNames = new ArrayList<String>(allContactsFromFile.keySet());
 		Collections.shuffle(contactNames);
-		incomingCallerInfo = allContactsCleaned.get(contactNames.get(0));
+		incomingCallerInfo = allContactsFromFile.get(contactNames.get(0));
 	}
 
-	/***
-	 * Helper method used to ensure we never use a key-value pair that has a
-	 * ContactInfo object where the phone number of that ContactInfo object is
-	 * blank, AKA "0", since we set blanks to 0 when reading in the data.
-	 * Intuitively, wouldn't make any sense to receive a call from a phone number of
-	 * "0" or blank.
-	 * 
-	 * @param allContacts hashMap we create upon reading in contacts from CSV
-	 * @return cleaned map of contacts used to generate calls (no blank phone #s)
-	 * 
-	 */
-	public HashMap<String, ContactInfo> removeBlankPhoneNumbersFromMapUsedToCreateCalls(
-			HashMap<String, ContactInfo> allContacts) {
-		/*
-		 * use iterator since we can't iterate over a keySet and remove key-value pair
-		 * simultaneously or java throws ConcurrentModificationException
-		 */
-
-		Iterator<Map.Entry<String, ContactInfo>> iterator = allContacts.entrySet().iterator();
-		while (iterator.hasNext()) {
-			Map.Entry<String, ContactInfo> nameWithContactInfo = iterator.next();
-			/*
-			 * if value (ContactInfo object) associated with key (name of contact) has
-			 * variable value for phone number as "0" then remove it from our map. Else keep
-			 * it.
-			 */
-			if (nameWithContactInfo.getValue().getPhoneNumber().equals("0")) {
-				iterator.remove();
-			}
-		}
-		return allContacts;
-
-	}
 
 	/***
 	 * Get contact information for the incoming caller
