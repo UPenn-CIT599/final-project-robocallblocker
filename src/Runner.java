46718eee1553aec04ea157fb2cfa27e4c2d6a192
Joshua Chopra <jchopra@seas.upenn.edu>
2019-12-03 00:37:00 -0500

    Added new runner class to execute the entire program.

---
+++ src/Runner.java
@@ -0,0 +1,25 @@
+import javax.swing.SwingUtilities;
+
+/***
+ * This class runs our entire CallBlockerProgram, using all of the classes: 
+ * 
+ * - Phone 
+ * - CallBlockerGUI 
+ * - ContactInfoReader 
+ * - ContactInfo 
+ * - IncomingCall 
+ * - SpamAlgorithm 
+ * - UsersContactList
+ * 
+ * @author JoshuaChopra
+ * @author ShawnChoudhury
+ * @author ThomasTee
+ *
+ */
+public class Runner {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new CallBlockerGUI_JoshEdits());
+	}
+
+}
