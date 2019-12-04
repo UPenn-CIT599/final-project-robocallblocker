a5922570ff6db1c4f84d3cf25e863f04d24dcd23
Joshua Chopra <jchopra@seas.upenn.edu>
2019-12-04 16:18:59 -0500

    used to test the new changes to the maps from Phone.java, confirms issue is resolved.

--- src/TestClass.java
+++ src/TestClass.java
@@ -8,10 +8,23 @@ public class TestClass {
 		ContactInfoReader allContactsInCSV = new ContactInfoReader("contacts100.csv");
 		HashMap<String, ContactInfo> allContactsMap = allContactsInCSV.getAllContactsInCSV();
 		System.out.println("Without phone class: " + allContactsMap + "\n");
-	
+		System.out.println("Number of contacts: " + allContactsMap.size() + "\n"); // same size as below phone.getAllContactsMap
+		
+		System.out.println("-------------" + "\n" + "Now using ContactInfo instance variable from phone class");
+		
 		
 		Phone phone = new Phone();
-		System.out.println("With phone class " + phone.getAllContactsMap());
+		System.out.println("With phone class " + phone.getAllContactsMap() + "\n");
+		System.out.println("Number of contacts: " + phone.getAllContactsMap().size() + "\n"); // same size as above allContactsMap
+		
+		System.out.println("--------");
+		phone.createIncomingCallDisplayOnPhoneScreenGUI(phone.getAllContactsMap());
+		System.out.println("After creating incoming call, which is done by calling the contactInfoReader's removeBlankPhoneNumbers method");
+
+		
+		System.out.println("Number of contacts: " + phone.getAllContactsInHashMapCleaned().size() + "\n");
+		
+		System.out.println("-----" + "\n" + "After cleaning the list from ContactInfoReader: " + allContactsMap.size());
 		
 
 	}
