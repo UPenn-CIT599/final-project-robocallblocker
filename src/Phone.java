16d9d5eb9dbaa0db5f41cb5cc025eaeee995077d
chShawn7 <chshawn@seas.upenn.edu>
2019-12-04 16:38:16 -0500

    Textfile output renamed to BlockList.txt

--- src/Phone.java
+++ src/Phone.java
@@ -121,7 +121,7 @@ public class Phone {
 		HashMap<String, String> blockList = spamAlgoForPhone.getBlockList();
 
 		try {
-			FileWriter fw = new FileWriter("FriendZoned.txt", true);
+			FileWriter fw = new FileWriter("BlockList.txt", true);
 			for (String name : blockList.keySet()) {
 				fw.write(name + ": " + blockList.get(name) + "\n");
 				fw.flush();
