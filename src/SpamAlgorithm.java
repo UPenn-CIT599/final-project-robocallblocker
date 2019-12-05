33ec7a40140123015ec87c79778f4df53678c50d
ttee9 <57302345+ttee9@users.noreply.github.com>
2019-12-04 16:13:17 -0800

    Update SpamAlgorithm.java
    Issue with score mapping not being able to handle socialmediahandles being person's first and last names

--- src/SpamAlgorithm.java
+++ src/SpamAlgorithm.java
@@ -29,16 +29,16 @@ public class SpamAlgorithm {
 	 */
 	private HashMap<String, Integer> mapForScoring(ContactInfo incoming) {
 		HashMap<String, Integer> scoreByAttribute = new HashMap<String, Integer>();
-		scoreByAttribute.put(incoming.getName(), 0);
-		scoreByAttribute.put(incoming.getPhoneNumber(), 0);
-		scoreByAttribute.put(incoming.getEmailAddress(), 0);
-		scoreByAttribute.put(incoming.getSocialMediaHandle(), 0);
-		scoreByAttribute.put(incoming.getAddress(), 0);
-		scoreByAttribute.put(incoming.getCompany(), 0);
-		scoreByAttribute.put(incoming.getCity(), 0);
-		scoreByAttribute.put(incoming.getCounty(), 0);
-		scoreByAttribute.put(incoming.getState(), 0);
-		scoreByAttribute.put(incoming.getZipCode(), 0);
+		scoreByAttribute.put(incoming.getName()+"0", 0);
+		scoreByAttribute.put(incoming.getCompany()+"1", 0);
+		scoreByAttribute.put(incoming.getAddress()+"2", 0);
+		scoreByAttribute.put(incoming.getCity()+"3", 0);
+		scoreByAttribute.put(incoming.getCounty()+"4", 0);
+		scoreByAttribute.put(incoming.getState()+"5", 0);
+		scoreByAttribute.put(incoming.getZipCode()+"6", 0);		
+		scoreByAttribute.put(incoming.getPhoneNumber()+"7", 0);
+		scoreByAttribute.put(incoming.getEmailAddress()+"8", 0);
+		scoreByAttribute.put(incoming.getSocialMediaHandle()+"9", 0);
 		return scoreByAttribute;
 	}
 	
@@ -69,37 +69,37 @@ public class SpamAlgorithm {
 		HashMap<String, Integer> scoreByAttribute = mapForScoring(incoming);
 		for (ContactInfo contact : phoneUsersContactMap.values()) {
 			if (incoming.getName().equals(contact.getName()) && !incoming.getName().equals("0")) {
-				scoreByAttribute.replace(incoming.getName(), 1);
+				scoreByAttribute.replace(incoming.getName()+"0", 1);
 			}
-			if (incoming.getEmailAddress().equals(contact.getEmailAddress())
-					&& !incoming.getEmailAddress().equals("0")) {
-				scoreByAttribute.replace(incoming.getEmailAddress(), 1);
+			if (incoming.getCompany().equals(contact.getCompany()) && !incoming.getCompany().equals("0")) {
+				scoreByAttribute.replace(incoming.getCompany()+"1", 1);
 			}
 			if (incoming.getAddress().equals(contact.getAddress()) && !incoming.getAddress().equals("0")) {
-				scoreByAttribute.replace(incoming.getAddress(), 1);
+				scoreByAttribute.replace(incoming.getAddress()+"2", 1);
 			}
-			if (incoming.getSocialMediaHandle().equals(contact.getSocialMediaHandle())
-					&& !incoming.getSocialMediaHandle().equals("0")) {
-				scoreByAttribute.replace(incoming.getSocialMediaHandle(), 1);
+			if (incoming.getCity().equals(contact.getCity()) && !incoming.getCity().equals("0")) {
+				scoreByAttribute.replace(incoming.getCity()+"3", 1);
+			}
+			if (incoming.getCounty().equals(contact.getCounty()) && !incoming.getCounty().equals("0")) {
+				scoreByAttribute.replace(incoming.getCounty()+"4", 1);
+			}
+			if (incoming.getState().equals(contact.getState()) && !incoming.getState().equals("0")) {
+				scoreByAttribute.replace(incoming.getState()+"5", 1);
+			}
+			if (incoming.getZipCode().equals(contact.getZipCode()) && !incoming.getZipCode().equals("0")) {
+				scoreByAttribute.replace(incoming.getZipCode()+"6", 1);
 			}
 			if (incoming.getPhoneNumber().equals(contact.getPhoneNumber()) && !incoming.getPhoneNumber().equals("0")) {
 				// 2 points if phone number is in user's phone
-				scoreByAttribute.replace(incoming.getPhoneNumber(), 2);
+				scoreByAttribute.replace(incoming.getPhoneNumber()+"7", 2);
 			}
-			if (incoming.getCompany().equals(contact.getCompany()) && !incoming.getCompany().equals("0")) {
-				scoreByAttribute.replace(incoming.getCompany(), 1);
+			if (incoming.getEmailAddress().equals(contact.getEmailAddress())
+					&& !incoming.getEmailAddress().equals("0")) {
+				scoreByAttribute.replace(incoming.getEmailAddress()+"8", 1);
 			}
-			if (incoming.getCity().equals(contact.getCity()) && !incoming.getCity().equals("0")) {
-				scoreByAttribute.replace(incoming.getCity(), 1);
-			}
-			if (incoming.getCounty().equals(contact.getCounty()) && !incoming.getCounty().equals("0")) {
-				scoreByAttribute.replace(incoming.getCounty(), 1);
-			}
-			if (incoming.getState().equals(contact.getState()) && !incoming.getState().equals("0")) {
-				scoreByAttribute.replace(incoming.getState(), 1);
-			}
-			if (incoming.getZipCode().equals(contact.getZipCode()) && !incoming.getZipCode().equals("0")) {
-				scoreByAttribute.replace(incoming.getZipCode(), 1);
+			if (incoming.getSocialMediaHandle().equals(contact.getSocialMediaHandle())
+					&& !incoming.getSocialMediaHandle().equals("0")) {
+				scoreByAttribute.replace(incoming.getSocialMediaHandle()+"9", 1);
 			}
 		}
 		return scoreByAttribute;
