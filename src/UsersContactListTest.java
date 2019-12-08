03c4a8e02b591d1141e7ad54af5d205b42d5a71b
Joshua Chopra <jchopra@seas.upenn.edu>
2019-12-08 14:46:03 -0500

    Added JavaDoc comments for class header

--- src/UsersContactListTest.java
+++ src/UsersContactListTest.java
@@ -6,31 +6,40 @@ import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 
+/***
+ * This class tests our UserContactsList class to ensure keys are properly
+ * shuffled in the HashMap containing contactInfo objects.
+ * 
+ * @author ShawnChoudhury
+ * @author JoshuaChopra
+ * @author ThomasTee
+ *
+ */
 class UsersContactListTest {
 
 	/**
-	 * Minor JUnit test to make sure the keys are shuffled
-	 * When the keys are processed into the HashMap from the ContactInfoReader
-	 * they should be shuffled; this way the initial sequence of keys are not repeated
-	 * This helps us create a random HashMap for the UsersContactList class
-	 * Initial key set should not match shuffled keyset
-	 * arraylist equals compares each corresponding element between 2 arraylists
+	 * Minor JUnit test to make sure the keys are shuffled When the keys are
+	 * processed into the HashMap from the ContactInfoReader they should be
+	 * shuffled; this way the initial sequence of keys are not repeated This helps
+	 * us create a random HashMap for the UsersContactList class Initial key set
+	 * should not match shuffled keyset arraylist equals compares each corresponding
+	 * element between 2 arraylists
 	 */
 	@Test
 	void testShuffleMapKeys() {
-		
+
 		ContactInfoReader reader = new ContactInfoReader("contacts10.csv");
 		HashMap<String, ContactInfo> map = reader.getAllContactsInCSV();
 		// keys from above map
 		ArrayList<String> keys1 = new ArrayList<>(map.keySet());
 		ArrayList<String> keys2 = keys1;
-		
-		
+
 		UsersContactList list = new UsersContactList(map, 10);
-		// shuffled keys; these should not be in same sequence as initial keys1 arraylist
+		// shuffled keys; these should not be in same sequence as initial keys1
+		// arraylist
 		ArrayList<String> shuffledKeys = list.shuffleMapKeys(map);
 		boolean isEqual = keys1.equals(shuffledKeys);
-		
+
 		assertEquals(false, isEqual);
 	}
 
